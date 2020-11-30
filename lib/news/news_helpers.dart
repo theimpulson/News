7c8a541b51be805f0401a2b77054640042232829
Aayush Gupta <aayushgupta219@gmail.com>
2020-11-30 20:12:53 +0530

    news: Implement news in homescreen

--- lib/news/news_helpers.dart
+++ lib/news/news_helpers.dart
@@ -1,6 +1,71 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class NewsHelpers {
+  /// Conatains various helper functions to work with newsapi.org
+
+  static const String api_url = 'https://newsapi.org/v2';
+  static const List news_type = ['top-headlines', 'everything'];
+  static const String default_api_url = '$api_url/top-headlines?country=in';
+  static const Map<String, String> country = {
+    'ae': 'United Arab Emirates',
+    'ar': 'Argentina',
+    'at': 'Austria',
+    'au': 'Australia',
+    'be': 'Belgium',
+    'bg': 'Bulgaria',
+    'br': 'Brazil',
+    'ca': 'Canada',
+    'ch': 'Switzerland',
+    'cn': 'China',
+    'co': 'Colombia',
+    'cu': 'Cuba',
+    'cz': 'Czech Republic',
+    'de': 'Germany',
+    'eg': 'Egypt',
+    'fr': 'France',
+    'gb': 'United Kingdom',
+    'gr': 'Greece',
+    'hk': 'Hong Kong',
+    'hu': 'Hungary',
+    'id': 'Indonesia',
+    'ie': 'Ireland',
+    'il': 'Israel',
+    'in': 'India',
+    'it': 'Italy',
+    'jp': 'Japan',
+    'kr': 'South Korea',
+    'lt': 'Lithuania',
+    'lv': 'Latvia',
+    'ma': 'Morocco',
+    'mx': 'Mexico',
+    'my': 'Malaysia',
+    'ng': 'Nigeria',
+    'nl': 'Netherlands',
+    'no': 'Norway',
+    'nz': 'New Zealand',
+    'ph': 'Philippines',
+    'pl': 'Poland',
+    'pt': 'Portugal',
+    'ro': 'Romania',
+    'rs': 'Serbia',
+    'ru': 'Russia',
+    'sa': 'Saudi Arabia',
+    'se': 'Sweden',
+    'sg': 'Singapore',
+    'si': 'Slovenia',
+    'sk': 'Slovakia',
+    'th': 'Thailand',
+    'tr': 'Turkey',
+    'tw': 'Taiwan',
+    'ua': 'Ukraine',
+    'us': 'United States',
+    've': 'Venuzuela',
+    'za': 'South Africa',
+  };
+
   Future<bool> apiKeyExists() async {
     /// Returns a bool indicating if API exists or not
 
@@ -14,5 +79,20 @@ class NewsHelpers {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     // ToDo: Add verification to check if API key is valid or not before saving it
     prefs.setString('api_key', key);
+  }
+
+  Future<dynamic> fetchNews() async {
+    /// Fetches & returns news from newsapi.org
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    final String apiKey = prefs.getString('api_key');
+    final reponse = await get(
+      default_api_url,
+      headers: {HttpHeaders.authorizationHeader: apiKey},
+    );
+    // ToDo: Handle errors with [reponse.statusCode] before returning
+    final jsonResponse = jsonDecode(reponse.body);
+    return jsonResponse['articles'];
   }
 }
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:News/news/news_helpers.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Homescreen extends StatelessWidget {
   const Homescreen({Key key}) : super(key: key);
@@ -6,8 +8,78 @@ class Homescreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
+      appBar: AppBar(),
+      body: FutureBuilder(
+        future: NewsHelpers().fetchNews(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot == null ? 0 : snapshot.data.length,
+                itemBuilder: (context, index) {
+                  String title = snapshot.data[index]['title'];
+                  String description = snapshot.data[index]['description'];
+                  String url = snapshot.data[index]['url'];
+                  String urlToImage = snapshot.data[index]['urlToImage'];
+                  String source = snapshot.data[index]['source']['name'];
+
+                  return Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(8.0),
+                        ),
+                      ),
+                      child: InkWell(
+                        onTap: () async {
+                          return await launch(url);
+                        },
+                        child: Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Column(
+                            children: <Widget>[
+                              Image.network(
+                                urlToImage,
+                                width: 350,
+                                height: 200,
+                                fit: BoxFit.fill,
+                              ),
+                              Text(''),
+                              Text(
+                                title,
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Text(description),
+                              Text(''),
+                              Row(
+                                children: <Widget>[
+                                  Text(
+                                    source,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                });
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          } else {
+            return Container(
+              alignment: AlignmentDirectional.center,
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
